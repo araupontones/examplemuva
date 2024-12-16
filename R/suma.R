10d98707cae28249a7a03f1125d966bc3619ff9d
araupontones <43657130+araupontones@users.noreply.github.com>
2024-12-16 09:50:56 +0100

    add description

---
+++ R/suma.R
@@ -0,0 +1,15 @@
+#'Suma dos numeros
+#'THis function takes two values x and y and sum them up
+#'
+#' @param x a number
+#'@param y a number
+#' @return the sum of x and y
+#' @export
+#' 
+
+suma <- function(x,y){
+  
+  x + y
+}
+
+
